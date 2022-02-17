c99066ff235f13050f1c8b6a425fcdc4c9fea829
Zain-mir24 <zainnaeemk10@gmail.com>
2022-02-17 15:42:29 +0500

    designed main body with span elements

---
+++ src/components/MainBody/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Body.css";
+function Body() {
+  return (
+    <div className="mainBackground">
+      <span className="spanElement">Nearest Rides</span>{" "}
+      <span className="spanElement">Nearest Rides</span>{" "}
+      <span className="spanElement">Nearest Rides</span>
+    </div>
+  );
+}
+
+export default Body;
