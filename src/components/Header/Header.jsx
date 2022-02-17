bb8e447b8369e7df7d4427973dde2ebb80072a37
Zain-mir24 <zainnaeemk10@gmail.com>
2022-02-17 13:09:08 +0500

    initial commmit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css"
+function Header() {
+  return (
+    <div className="container-fluid">
+      <div className="row header ">
+          <div className="col-lg-6  leftHeading">
+          Edvora
+          </div>
+          <div  className="col-lg-6">
+              <div className="col-lg-10  rightHeading">
+              Zainmir 
+              </div>
+              <div className="col-lg-2">
+
+              </div>
+            
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
