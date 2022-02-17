a929de6c1ddf89b8a4d38ff9f6a319ec3f0fbf29
Zain-mir24 <zainnaeemk10@gmail.com>
2022-02-17 13:23:47 +0500

    header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
 function Header() {
   return (
     <div className="container-fluid">
       <div className="row header ">
-          <div className="col-lg-6  leftHeading">
-          Edvora
+        <div className="col-lg-6  leftHeading">Edvora</div>
+        <div className="col-lg-6">
+          <div className="  rightHeading">
+            Zainmir <img className="rectangle" src={"./myimage.jpg"} />
           </div>
-          <div  className="col-lg-6">
-              <div className="col-lg-10  rightHeading">
-              Zainmir 
-              </div>
-              <div className="col-lg-2">
-
-              </div>
-            
-          </div>
+        </div>
       </div>
     </div>
   );
